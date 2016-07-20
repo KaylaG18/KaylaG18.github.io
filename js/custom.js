4f896ff44b2ff42a259e495d7fa4e7a10a698056
KaylaG18 <adiamonique2018@gmail.com>
2016-07-20 15:47:14 -0700

    add all files

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	$("p").click(function(){
+		$(this).hide();
+
+	});	
+
+
+	$("h2").click(function(){
+		$(this).hide();
+	});
+
+	$("h3").click(function(){
+		$(this).hide();
+	});
+
+	$("h4").click(function(){
+		$(this).hide();
+	});
+
+	$("h5").click(function(){
+		$(this).hide();
+	});
+})	
+
+
+
+	
+
