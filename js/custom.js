70f662753335f9213b660d9d6056d6146e1f35ba
KaylaG18 <adiamonique2018@gmail.com>
2016-07-21 10:38:31 -0700

    add all files

--- js/custom.js
+++ js/custom.js
@@ -4,23 +4,33 @@ $(document).ready(function() {
 
 	});	
 
+	// $("h3").click(function(){
+	// 	$(this).hide();
+	// });
 
-	$("h2").click(function(){
-		$(this).hide();
-	});
+	// $("h4").click(function(){
+	// 	$(this).hide();
+	// });
+
+	// $("h5").click(function(){
+	// 	$(this).hide();
+	// });
 
 	$("h3").click(function(){
-		$(this).hide();
+		$(this).text("If anyone needs to contact me for my services, feel free to email me @kamg2018@gmail.com. Serious busuiness inquiries only!");
 	});
 
 	$("h4").click(function(){
-		$(this).hide();
+		$(this).text("My name is Kayla Green. I was born and mostly raised is Sacramento, CA. I have two siblings and I enjoy being on the phone. I also enjoy ejoy eating. My favorite place would be my room because I would be away from other people!");
 	});
 
 	$("h5").click(function(){
-		$(this).hide();
+		$(this).text("One of my favorite projects was making the arduinio robots dance. The robot I worked on was TEK and it did the cupid shuffle. Another favorite project of mine was the Madlibs.");
 	});
 })	
+
+
+	
 
 
 
